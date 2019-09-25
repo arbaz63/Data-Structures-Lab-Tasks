9cd2a9657b51f2a14ba275eb2347150908976e6c
Ghulam Ghous <itsghulamghous@gmail.com>
2019-09-26 02:13:13 +0500

    First commit

---
+++ Link_List_Lab3/Link_List_Lab3/main.cpp
@@ -0,0 +1,18 @@
+//This program is a main for list.cpp and node.cpp
+
+#include "list.cpp"
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	//Declared an object of list type
+	list linkList;
+	//inserting nodes in the link list
+	linkList.insert(100);
+	linkList.insert(150);
+	linkList.insert(200);
+	linkList.insert(1000);
+}
