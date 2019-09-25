0bf8bc49ea65f364eebe3b1cc86e2bc308f4e8f4
Ghulam Ghous <itsghulamghous@gmail.com>
2019-09-26 02:12:51 +0500

    First commit

---
+++ Link_List_Lab3/Link_List_Lab3/list.cpp
@@ -0,0 +1,99 @@
+//This Program contains the list class. We declared insert, insert at end, insert at beginning, update value by index,
+//update value by using previous value and print the link list
+
+//Added node.cpp file
+#include "node.cpp"
+#include <iostream>
+
+using namespace std;
+
+//Declared class list in which we defined methods we want to implement on Link List
+class list {
+
+	//Pointer which has the address of top 
+	node *headNode;
+
+	//pointer which points to the current Node
+	node *currentNode;
+
+	//pointer which points to the Last Current Node
+	node *lastCurrentNode;
+
+	//To keep record of size of Link List
+	int size;
+
+public:
+
+	//Default Constructor which Sets all pointers to NULL and size to zero
+	list() {
+		headNode = NULL;
+		currentNode = NULL;
+		lastCurrentNode = NULL;
+		size = 0;
+	}
+
+	//Function to insert a new node in the link list
+	void insert(int number) {
+		if (currentNode != NULL)
+		{
+			if (currentNode->getNextNode() == 0) {
+				node *temp = new node();
+				currentNode->setNextNode(temp);
+				lastCurrentNode = currentNode;
+				currentNode = temp;
+				currentNode->setValue(number);
+				currentNode->setNextNode(0);
+				size++;
+			}
+			else {
+				node *temp = new node();
+				temp->setNextNode(currentNode->getNextNode());
+				currentNode->setNextNode(temp);
+				lastCurrentNode = currentNode;
+				currentNode = temp;
+				currentNode->setValue(number);
+				size++;
+
+			}
+		}
+		else {
+			node *temp = new node();
+			headNode = temp;
+			currentNode = headNode;
+			lastCurrentNode = 0;
+			currentNode->setValue(number);
+			currentNode->setNextNode(0);
+			size++;
+		}
+	}
+
+	//Function to get size of Link List
+	int getSize() {
+		return size;
+	}
+
+	//Function to get value of a specific node
+	int get() {
+		return currentNode->getValue();
+	}
+
+	//Function to get Value of Last current Node
+	int getLast() {
+		return lastCurrentNode->getValue();
+	}
+
+	//Function to move at the start of the Link List
+	void start() {
+		lastCurrentNode = 0;
+		currentNode = headNode;
+	}
+
+
+	//Function to move to next Node
+	void move() {
+		if (currentNode->getNextNode() != 0) {
+			lastCurrentNode = currentNode;
+			currentNode = currentNode->getNextNode();
+		}
+	}
+};
