f20285a8e27c48c0b7efa408778c371df654c268
Ghulam Ghous <itsghulamghous@gmail.com>
2019-09-26 02:29:15 +0500

    Added code to call function updateValue by using previous value and print function

--- Link_List_Lab3/Link_List_Lab3/main.cpp
+++ Link_List_Lab3/Link_List_Lab3/main.cpp
@@ -10,6 +10,7 @@ int main()
 {
 	//Declared an object of list type
 	list linkList;
+
 	//inserting nodes in the link list
 	linkList.insert(100);
 	linkList.insert(150);
@@ -17,21 +18,32 @@ int main()
 	linkList.insert(1000);
 	cout << "Now my Link List is" << endl;
 	linkList.print();
+
 	//inserting a node having value of 50 at the start of the Link List
 	linkList.insertAtBegin(50);
 	cout << "/nNow after adding 50 at the beginning my Link List is" << endl;
 	linkList.print();
+
 	//inserting a node having value of 5 at the start of the Link List
 	linkList.insertAtBegin(5);
 	cout << "/nNow after adding 5 at the beginning my Link List is" << endl;
 	linkList.print();
+
 	//inserting a node having value of 1100 at the end of the Link List
 	linkList.insertAtEnd(1100);
 	cout << "/nNow after adding 50 at the end my Link List is" << endl;
 	linkList.print();
+
 	//Updated a node having value of 200 at the 5th index in the list by new value 201
 	linkList.updateValueByIndex(201, 5);
 	//Printing the list after updation
 	cout << "/nNow after Updating a node having value of 200 at the 5th index in the list by new value 201 my Link List is" << endl;
 	linkList.print();
+
+	//Updating a node having previous value of 150 in the list by new value 151
+	linkList.updateValue(151, 150);
+	cout << "Now after Updating a node having previous value of 150 in the list by new value 151 my Link List is" << endl;
+	linkList.print();
+
+	system("pause");
 }
