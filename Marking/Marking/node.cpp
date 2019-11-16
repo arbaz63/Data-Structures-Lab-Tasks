fdc84c211c2769c16940b95f104b88be4f6036d2
ghulamghousdev <itsghulamghous@gmail.com>
2019-11-16 13:12:06 +0500

    📦 NEW: first commit

---
+++ Marking/Marking/node.cpp
@@ -0,0 +1,67 @@
+//Implementation file of node class
+
+#pragma once
+#include <iostream>
+#include <string>
+#include "node.h"
+using namespace std;
+
+
+//default constructor
+node::node()
+{
+	nextNode = NULL;
+	data = 0;
+	remarks = "";
+	registrationNumber = "";
+
+}
+
+
+//Function used to set data passed by the user in node of STACK
+node& node::setData(int number)
+{
+	this->data = number;
+	return *this;
+}
+
+
+//Function used to get data and return it from a node of STACK
+int node::getData()
+{
+	return data;
+}
+
+//Function to set next node
+void node::setNextNode(node* ptr)
+{
+	nextNode = ptr;
+}
+//Function to get next node
+node* node::getNextNode()
+{
+	return nextNode;
+}
+
+void node::setName(string reg) {
+	registrationNumber = reg;
+}
+
+void node::setRemarks()
+{
+	remarks = "Late Submission";
+}
+
+//Print function is used to print all the elements of the STACK
+void node::print() const
+{
+	cout << data;
+}
+
+void node::printName() const {
+	cout << registrationNumber;
+}
+
+void node::printRemarks() const {
+	cout << remarks << " ";
+}
